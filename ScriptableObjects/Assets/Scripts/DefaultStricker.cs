02edcfde90e6b0a7bbc86eba88ee042ce1545b1a
manikarna <97171564+manikarna19@users.noreply.github.com>
2022-06-15 15:27:48 +0530

    15:27

--- ScriptableObjects/Assets/Scripts/DefaultStricker.cs
+++ ScriptableObjects/Assets/Scripts/DefaultStricker.cs
@@ -2,8 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Invenory :MonoBehaviour
+public class DefaultStricker : Item
 {
-
-
+   
 }
---
+++ ScriptableObjects/Assets/Scripts/Item.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[CreateAssetMenu()]
+public  class Item :ScriptableObject
+{
+    public GameObject Stricker;
+    public Texture ItsTexture;
+    public float AimingHelp;
+    public void OnThisStrickerSelected()
+    {
+
+    }
+}
